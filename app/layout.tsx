9ec704ee269188df06ce03d5b2bbdc5fa4e0841e
jayden-dragon <gsjang95@gmail.com>
2026-02-11 15:59:50 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Gyusam Chang | AI Researcher',
   description: 'PhD Student in AI at Korea University',
+  icons: {
+    icon: 'app/favicon.png', 
+  },
 };
 
 export default function RootLayout({
